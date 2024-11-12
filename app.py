bc58d8af6532d27dbfd7abe2aca79330f593e69d
ohgeezman-joaquin <haca9871jk@gmail.com>
2024-11-12 14:38:41 +0800

    Add project files

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, abort
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
+from datetime import datetime
+
+app = Flask(__name__)
+
+configuration = Configuration(access_token='')
+handler = WebhookHandler('')
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers.get('X-Line-Signature')
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    reply_text = event.message.text
+    if reply_text == "給我今天日期":
+        reply_text = datetime.now().strftime("%Y-%m-%d")
+
+
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=reply_text)]
+            )
+        )
+
+if __name__ == "__main__":
+    app.run(port=8080)
